e3a635d4d4d91733d17b9d6e09d528c12c3dcb11
Deepak Kumar Singh <deepakks1595@gmail.com>
2020-07-06 00:35:01 +0530

    correct solution

--- codechef/july_long_2020/ptmssng.cpp
+++ codechef/july_long_2020/ptmssng.cpp
@@ -2,46 +2,16 @@
 using namespace std;
 
 void handleCase() {
-	int n = 0, x = 0, y = 0;
-	map<long, long> xmap, ymap;
-	std::map<long, long>::iterator itr;
+	int n = 0, x = 0, y = 0, xans = 0, yans = 0;
 
 	cin >> n;
 	for (int i = 0; i < 4 * n - 1; i++) {
 		cin >> x >> y;
-
-		itr = xmap.find(x);
-		if (itr == xmap.end() || itr -> second == 0) {
-			xmap[x] = 1;
-		}
-		else {
-			xmap[x] = 0;
-		}
-
-		itr = ymap.find(y);
-		if (itr == ymap.end() || itr -> second == 0) {
-			ymap[y] = 1;
-		}
-		else {
-			ymap[y] = 0;
-		}
+		xans ^= x;
+		yans ^= y;
 	}
 
-	for (itr = xmap.begin(); itr != xmap.end(); itr++) {
-		if (itr -> second == 1) {
-			x = itr -> first;
-		}
-	}
-
-
-
-	for (itr = ymap.begin(); itr != ymap.end(); itr++) {
-		if (itr -> second == 1) {
-			y = itr -> first;
-		}
-	}
-
-	cout << x << " " << y << endl;
+	cout << xans << " " << yans << endl;
 	return;
 }
 
